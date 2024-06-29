0458156715f8e8a16a5bc3d101fe89144860094c
Peter Huang <huangcx22@mails.tsinghua.edu.cn>
2024-06-29 20:47:45 +0800

    Checkpoint 24-06-29-20-47

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -33,8 +33,8 @@ function vec(x, y) {
  */
 function h(tag, props, children) {
   const element = document.createElement(tag);
-  Object.keys(props).forEach((key) => {
-    element.setAttribute(key, props[key]);
+  Object.keys(props).forEach((name) => {
+    element.setAttribute(name, props[name]);
   });
   children.forEach((child) => {
     element.appendChild(child);
@@ -46,13 +46,18 @@ function t(text) {
   return document.createTextNode(text);
 }
 
-// a better way: each key in an ARK object holds metadata and data about this slot
-// obj.key.value <=> obj.send(key)
+// a better way: each name in an ARK object holds metadata and data about this slot
+// obj.name.value <=> obj.send(name)
 // set all other objects' prototypes as the warehouse, and add functions to the warehouse
-// then the message menu is just Object.keys(obj).map(key => obj[key])...
+// then the message menu is just Object.keys(obj).map(name => obj[name])...
 
 const ARKObjectDoc = `
-An ARK object is a js object displayed in the world, annotated with information, such as:
+\`ARKObject\` is used to create objects in ARK.
+
+An ARK object is a js object where each value is a "slot object",
+with a value and metadata about the slot. 
+
+Each slot is annotated with information, such as:
 
 - a name 
 - a **mandatory** markdown doc string
@@ -60,9 +65,8 @@ An ARK object is a js object displayed in the world, annotated with information,
 - a list of information about messages it understands
   - this is used to construct buttons
 
-A Visual ARK object also knows:
-- its position in the world
-- an HTML element to represent it
+Any JS object can be annotated and transformed into an ARK object.
+The original object is stored in the \`__original__\` field.
 
 These are available at runtime, so users can view the documentation of any object when the program is running.
 
@@ -72,6 +76,111 @@ There are several essential messages that every ARK object must understand:
 - XEROX, clones the object
 - messages, returns the list of messages the object understands
 
+An interactor should also understand \`step\`, a message sent to it every tick.
+`;
+
+const PreARKObject = {
+  new() {
+    return Object.create(this);
+  },
+  addSlot(name, doc, value) {
+    this.addMethod(name, "unary", doc, () => value);
+    return this;
+  },
+  addSlotMut(name, getterDoc, setterDoc, value) {
+    // crazy hack: store the value in a closure
+    let store = value;
+    this.addMethod(name, "unary", getterDoc, () => store);
+    this.addMethod(`${name}:`, "keyword", setterDoc, function (v) {
+      store = v;
+      return this;
+    });
+    return this;
+  },
+  addMethod(name, type, doc, fn) {
+    if (type !== "unary" && type !== "binary" && type !== "keyword") {
+      throw new Error(
+        `Invalid type: ${type}.\nAllowed types are "unary", "binary" and "keyword".`
+      );
+    }
+    if (typeof fn !== "function") {
+      throw new Error(`${fn} should be a function.`);
+    }
+    this[name] = { type, doc, value: fn.bind(this) };
+    return this;
+  },
+  send(name, ...args) {
+    if (!this[name]) {
+      throw new Error(`Object does not understand message "${name}".`);
+    }
+    const type = this[name].type;
+    if (type !== "unary" && type !== "binary" && type !== "keyword") {
+      throw new Error(
+        `Cannot send message "${name}" of type "${type}".\nAllowed types are "unary", "binary" and "keyword".`
+      );
+    }
+    const arity =
+      this[name].type === "unary"
+        ? 0
+        : this[name].type === "binary"
+        ? 1
+        : name.split(":").length - 1;
+
+    if (args.length != arity) {
+      throw new Error(`Expected ${arity} arguments, got ${args.length}.`);
+    }
+    return this[name].value(...args);
+  },
+};
+
+function wrap(obj, objDoc, docs) {
+  const wrapped = Object.create(PreARKObject);
+  wrapped.__original__ = obj;
+  wrapped.addSlot("doc", "The documentation of the object.", objDoc);
+  Object.keys(docs).forEach((name) => {
+    const metadata = docs[name];
+    const methodName = name.split(":")[0];
+    wrapped.addMethod(name, metadata.type, metadata.doc, obj[methodName]);
+  });
+  return wrapped;
+}
+
+const ARKObject = wrap(PreARKObject, ARKObjectDoc, {
+  new: {
+    type: "unary",
+    doc: `Create a new ARK object.`,
+  },
+  "addSlot:doc:initially:": {
+    type: "keyword",
+    doc: `Add a slot to the ARK object with documentation and an initial value.`,
+  },
+  "addSlotMut:getterDoc:setterDoc:initially:": {
+    type: "keyword",
+    doc: `Add a mutable slot to the ARK object with documentation for a getter and a setter, and an initial value.`,
+  },
+  "addMethod:ofType:doc:fn:": {
+    type: "keyword",
+    doc: `Add a method to the ARK object with a type, documentation, and a function as its implementation.`,
+  },
+});
+
+function logThis() {
+  console.log(this);
+}
+
+const test1 = ARKObject.send("new").addMethod(
+  "logThis",
+  "unary",
+  `A method that logs the "this" variable.`,
+  logThis
+);
+test1.send("logThis");
+
+const ARKVisualDoc = `
+A Visual ARK object knows:
+- its position in the world
+- an HTML element to represent it
+
 A Visual ARK object should also understand:
 
 - vaporize, removes the object from the world
@@ -79,7 +188,6 @@ A Visual ARK object should also understand:
 - get position, returns the position of the object as a Vector
 - set position, sets the position of the object
 
-An interactor should also understand \`step\`, a message sent to it every tick.
 `;
 
 /*
